67074d060089f5724c738835979ce1011b405a0e
Shira Biton <shirabiton387@gmail.com>
2025-01-20 07:14:45 +0200

    design list item

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -2,8 +2,10 @@ import { FC, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import Item from "../types/Item";
 import { ItemsContext } from "../App";
+import useStyles from "./styles";
 
 const ItemList: FC = () => {
+    const classes = useStyles();
     const navigate = useNavigate();
     const items: Item[] = useContext(ItemsContext);
     const screenWidth = window.screen.width;
@@ -11,21 +13,22 @@ const ItemList: FC = () => {
     const isSlowConnection = ["2g", "3g", "slow-2g"].includes(connectionType);
 
 
-    return <div>
-        <h1>Item list</h1>
+    return <>
         <ul>
             {items && items.map((item, index) => {
                 const imageQuality = isSlowConnection ? item.thumbnails.lowQuality : item.thumbnails.highQuality;
                 return (
                     <li key={index} onClick={() => navigate(`${index}`)}>
-                        <p>{item.title}</p>
+                        <h1>{item.title}</h1>
                         <p>{item.description}</p>
-                        <p>Publisher: {item.metaData.publisher}</p>
-                        <p>Date: {item.metaData.date}</p>
                         <img src={screenWidth > 1024 ? imageQuality.highResolution : imageQuality.lowResolution} alt={item.title} title={item.title} style={{ width: '200px', height: 'auto' }} />
+                        <span className={classes.metaData}>
+                            <p>{item.metaData.publisher}</p>
+                            <p>{item.metaData.date}</p>
+                        </span>
                     </li>)
             })}
         </ul>
-    </div>
+    </>
 }
 export { ItemList }
--- src/components/styles.ts
+++ src/components/styles.ts
@@ -3,15 +3,43 @@ import { createUseStyles } from 'react-jss'
 export const useStyles = createUseStyles({
   '@global': {
     body: {
-      direction: 'rtl'
-    }
+      direction: 'rtl',
+      margin: 0,
+      padding: 0,
+      fontFamily: 'system-ui',
+      fontSize: '1vw'
+    },
+    h1: {
+      fontSize: '1.5vw'
+    },
+    ul: {
+      listStyle: 'none',
+      display: 'flex',
+      flexWrap: 'wrap',
+      gap: '5vw',
+      marginRight: 'auto'
+    },
+    li: {
+      width: '25vw',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      cursor: 'pointer',
+      borderRight: '0.5px #D3D0CB solid',
+      boxSizing: 'border-box'
+
+    },
   },
   header: {
     backgroundColor: 'black',
     color: 'white'
   },
-  itemList: {
-
+  metaData: {
+    width: '60%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    color: '#565B5E',
+    fontSize: '2vh'
   }
 })
 export default useStyles;
